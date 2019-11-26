48bd94b1588d5c99698310e2ba78fe9c5ad12359
Agung Triyadi <agung.triyadi@students.amikom.ac.id>
2019-11-26 21:31:11 +0700

    Membuat input output pada python

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello world')
+'''a = 5
+nama = 'yoggu'
+print('Nilai a adalah ', a)
+print('Nama dia adalah %s'% nama)
+print('Atas nama %s memiliki hasil nilai %d'% (nama, a))
+'''
+num =  input('masukkan umur kamu : ')
+print('umur kamu adalah %s tahun '% num)
